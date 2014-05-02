ad19052fb8a2ebb71b5583f19cc7c527d056d664
StoraH <hjalmar.skoldefors@wunderkraut.com>
2014-05-02 17:12:32 +0200

    DOT support and D3 insted of vis

--- anatomy.js
+++ anatomy.js
@@ -3,58 +3,15 @@
 Drupal.behaviors.anatomy = {
   attach: function (context, settings) {
 
-    // create an array with nodes
-    var nodes = settings.anatomy.nodes;
+    // Get graph data
+    var inputGraph = document.querySelector("#inputGraph"),
 
-    // create an array with edges
-    var edges = settings.anatomy.edges;
+    // Parse graph data
+    result = graphlibDot.parse(inputGraph.value),
 
-    var container = document.getElementById('anatomy');
-    var data = {
-      nodes: nodes,
-      edges: edges,
-    };
-
-    var options = {
-      width:  '100%',
-      height: '600px',
-      smoothCurves: false,
-      stabilize: false,
-
-      nodes: {
-        radiusMin: 16,
-        radiusMax: 32,
-        fontColor: 'BLACK'
-      },
-
-      edges: {
-        color: 'GRAY'
-      },
-
-      physics: {
-          barnesHut: {
-              enabled: true,
-              gravitationalConstant: -2000,
-              centralGravity: 0.1,
-              springLength: 95,
-              springConstant: 0.04,
-              damping: 0.09
-          },
-          repulsion: {
-              centralGravity: 0.1,
-              springLength: 50,
-              springConstant: 0.05,
-              nodeDistance: 100,
-              damping: 0.09
-          },
-      }
-
-
-
-    };
-
-    var timeline = new vis.Graph(container, data, options);
-
+    // Render graph
+    renderer = new dagreD3.Renderer();
+    renderer.run(result, d3.select("#anatomy g"));
 
   }
 };
--- anatomy.tpl.php
+++ anatomy.tpl.php
@@ -1,1 +1,14 @@
-<div id="anatomy"></div>
+<div class="Anatomy">
+
+  <svg class="Anatomy-graph" id="anatomy" width=650 height=680>
+      <g transform="translate(20,20)"/>
+  </svg>
+
+  <h2>Anatomy data</h2>
+  <textarea class="Anatomy-input" id="inputGraph">
+    digraph graphname {
+    <?php print $data?>
+     }
+  </textarea>
+
+</div>
